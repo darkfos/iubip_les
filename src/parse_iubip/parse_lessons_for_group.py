cd0b6f102bf932f09f6339d24973b4f7dd540778
fos <darkfos82@gmail.com>
2024-01-27 03:03:31 +0300

    Добавил метод, для получения всех пар по указанной группе

--- src/parse_iubip/parse_lessons_for_group.py
+++ src/parse_iubip/parse_lessons_for_group.py
@@ -38,6 +38,27 @@ class Lessons:
         else:
 
             return None
+        
+    
+    async def get_all_lessons_for_group(self) -> list:
+        """
+            Получаем все необходимые данные о всех парах.
+        """
+        res_json = await self.parse_all_lessons()
+        
+        all_lessons: list = list()
+
+        for item in res_json[self.name_group][1]:
+            for day in res_json[self.name_group][1][item][1]:
+                for lessons in res_json[self.name_group][1][item][1].get(day):
+                    result: list = list(res_json[self.name_group][1][item][1].get(day).get(lessons))[0]
+                    message = f"Занятие №: {result.get("LES")}\nАудитория: {result.get("AUD")}\nПреподаватель: {result.get("NAME")}\nКафедра: {result.get("CAFEDRA")}\nДата: {result.get("DATE")}\nКурс: {result.get("COURSE")}"
+                    all_lessons.append(message)
+
+                all_lessons.append("\n")
+
+        return all_lessons
+                
 
 
-asyncio.run(Lessons("К1Т1(9)").parse_all_lessons())
+#asyncio.run(Lessons("К1Т1(9)").get_all_lessons_for_group())
