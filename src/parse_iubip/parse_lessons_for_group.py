ce91e408da4081c667f8851e28d9b1d33f27316b
fos <darkfos82@gmail.com>
2024-01-29 15:06:01 +0300

    Добавил метод на получения расписания пар на текущий день

--- src/parse_iubip/parse_lessons_for_group.py
+++ src/parse_iubip/parse_lessons_for_group.py
@@ -1,6 +1,7 @@
 import sys, os
 import json
 import asyncio
+import datetime
 
 sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 
@@ -19,6 +20,7 @@ class Lessons:
         self.name_group = name_group
         self.connection = HTMLSession()
     
+
     async def parse_all_lessons(self) -> None | list:
         """
             Получение расписания всех пар в учебном заведении - ИУБИП.
@@ -44,6 +46,7 @@ class Lessons:
         """
             Получаем все необходимые данные о всех парах.
         """
+
         res_json = await self.parse_all_lessons()
         
         all_lessons: list = list()
@@ -54,10 +57,35 @@ class Lessons:
                     result: list = list(res_json[self.name_group][1][item][1].get(day).get(lessons))[0]
                     message = f"Пара №: {result.get("LES")}\nПредмет: {result.get("SUBJECT").rstrip()}\nАудитория: {result.get("AUD")}\nПреподаватель: {result.get("NAME")}\nКафедра: {result.get("CAFEDRA")}\nДата: {result.get("DATE")}\nКурс: {result.get("COURSE")}\n"
                     all_lessons.append(message)
+                    print(message)
                 all_lessons.append("\n")
 
         return all_lessons
+    
+
+    async def get_now_lessons(self, now_day: str = str(datetime.datetime.now().day)) -> list | str:
+        """
+            Получение всех пар на текущий день
+        """
+
+        res_json = await self.parse_all_lessons()
+        
+        all_lessons: list = list()
+
+        for item in res_json[self.name_group][1]:
+            for day in res_json[self.name_group][1][item][1]:
+                for lessons in res_json[self.name_group][1][item][1].get(day):
+                    result: list = list(res_json[self.name_group][1][item][1].get(day).get(lessons))[0]
+                    if now_day in result.get("DATE"):
+                        message = f"Пара №: {result.get("LES")}\nПредмет: {result.get("SUBJECT").rstrip()}\nАудитория: {result.get("AUD")}\nПреподаватель: {result.get("NAME")}\nКафедра: {result.get("CAFEDRA")}\nДата: {result.get("DATE")}\nКурс: {result.get("COURSE")}\n"
+                        all_lessons.append(message)
+                        all_lessons.append("\n")
+
+        if all_lessons:
+            return all_lessons
+        return "Расписание пар на сегодня отсутствуют"
+    
                 
 
 
-asyncio.run(Lessons("К1Т1(9)").get_all_lessons_for_group())
+asyncio.run(Lessons("К2Л1(11)").get_now_lessons())
